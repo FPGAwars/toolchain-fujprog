1e9bbafe599e40ae4fcb2f0a8d7891adcc7c3168
Obijuan <juan@iearobotics.com>
2020-10-13 09:46:17 +0200

    Initial clean script added

---
+++ clean.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# -- Target architectures
+ARCH=$1
+TARGET_ARCHS="linux_x86_64 windows_amd64 darwin"
+
+# -- Store current dir
+WORK_DIR=$PWD
+
+# -- Folder for storing the downloaded packages
+PACKAGES_DIR=$WORK_DIR/_packages
+
+# -- Check ARCH
+if [[ $# > 1 ]]; then
+  echo ""
+  echo "Error: too many arguments"
+  exit 1
+fi
+
+if [[ $# < 1 ]]; then
+  echo ""
+  echo "Usage: bash clean.sh TARGET"
+  echo ""
+  echo "Targets: $TARGET_ARCHS"
+  exit 1
+fi
+
+if [[ $ARCH =~ [[:space:]] || ! $TARGET_ARCHS =~ (^|[[:space:]])$ARCH([[:space:]]|$) ]]; then
+  echo ""
+  echo ">>> WRONG ARCHITECTURE \"$ARCH\""
+  exit 1
+fi
+
+echo ""
+echo ">>> ARCHITECTURE \"$ARCH\""
+
+printf "Are you sure? [y/N]:${NC} "
+read RESP
+case "$RESP" in
+    [yY][eE][sS]|[yY])
+
+      # -- Directory for installation the target files
+      PACKAGE_DIR=$PACKAGES_DIR/build_$ARCH
+
+      # -- Remove the package dir
+      rm -r -f $PACKAGE_DIR
+
+      echo ""
+      echo ">> CLEAN"
+      ;;
+    *)
+      echo ""
+      echo ">> ABORT"
+      ;;
+esac
